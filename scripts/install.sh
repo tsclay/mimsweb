213acbbf740f1fe59879139d6d1ebab6c4824961
Tim Clay <tsclay9@gmail.com>
2020-11-15 13:24:47 -0500

    create pkg install script for fresh 'git clones'

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "📦 Installing NPM packages 📦"
+cd client && npm i
+cd ../server/static/js && npm i
+
+cd ../../
+echo "🐍 Setting Python venv & installing pkgs 🐍"
+python3 -m venv ./venv
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
