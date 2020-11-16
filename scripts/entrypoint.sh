2b16eb9d433539b07f7b02127470bb788f87e3ce
Tim <tsclay9@gmail.com>
2020-11-16 09:42:34 -0500

    build Svelte app in prod

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]; then
+  echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
 
-    echo "🐘 PostgreSQL started 🐘"
+  echo "🐘 PostgreSQL started 🐘"
 fi
 
-if [ "$FLASK_ENV" = "development" ]
-then
-    python manage.py create_db
-    echo "💻  Dev database created! 💻"
-    python manage.py seed_db
-    echo "🌱 Database seeded 🌱"
-    flask run
+if [ "$FLASK_ENV" = "development" ]; then
+  python manage.py create_db
+  echo "💻  Dev database created! 💻"
+  python manage.py seed_db
+  echo "🌱 Database seeded 🌱"
+  flask run
 fi
 
-if [ "$FLASK_ENV" = "production" ]
-then
-    python manage.py create_db
-    echo "💻  Dev database created! 💻"
-    python manage.py seed_db
-    echo "🌱 Database seeded 🌱"
-    gunicorn -b $FLASK_RUN_HOST:${FLASK_RUN_PORT} 'server:create_app()'
+if [ "$FLASK_ENV" = "production" ]; then
+  cd client && npm run build
+  echo "🚀 Svelte app bundled 🚀"
+  cd ..
+  sleep 0.1
+  python manage.py create_db
+  echo "💻  Dev database created! 💻"
+  python manage.py seed_db
+  echo "🌱 Database seeded 🌱"
+  gunicorn -b $FLASK_RUN_HOST:${FLASK_RUN_PORT} 'server:create_app()'
 fi
 
 exec "$@"
