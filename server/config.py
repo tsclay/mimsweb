a5e268b8c48118f8c1bb297ccabbaa70b7b9ef57
Tim Clay <tsclay9@gmail.com>
2020-11-17 01:39:32 -0500

    use https on redirects

--- server/config.py
+++ server/config.py
@@ -6,6 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ["SECRET_KEY"]
+    PREFERRED_URL_SCHEME = 'https'
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'server/static/assets'
