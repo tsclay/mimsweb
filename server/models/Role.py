d0e1c4c470ea2c9bbe95f3615e22d18e6dc0d419
Tim <tsclay9@gmail.com>
2020-11-22 15:20:05 -0500

    create role mode

--- server/models/Role.py
+++ server/models/Role.py
@@ -1,1 +1,16 @@
 from server.db import db
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(128), unique=True, nullable=False)
+    permissions = db.Column(db.ARRAY(db.String(128)), nullable=False)
+
+    def __init__(self, role_name, permissions):
+        self.role_name = role_name
+        self.permissions = permissions
+
+
+# SELECT admins.*, roles.permissions FROM admins INNER JOIN roles ON admins.role = roles.role_name;
