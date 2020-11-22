84a51d4d6bf84d042c8492f339dcbd44939f1649
Tim <tsclay9@gmail.com>
2020-11-22 15:19:38 -0500

    update seed data

--- manage.py
+++ manage.py
@@ -5,18 +5,36 @@ from server.models.Admin import Admin
 from server.models.Header import Header
 from server.models.Image import Image
 from server.models.Paragraph import Paragraph
+from server.models.Gallery import Gallery
+from server.models.Role import Role
 
 cli = FlaskGroup(server)
 
 seed = []
 
-image = Image(image_link="/static/assets/image1.jpg",
-              image_name="waterfall")
-seed.append(image)
+image1 = Image(image_link="/static/assets/image1.jpg",
+               image_name="waterfall")
+
+image2 = Image(image_link="/static/assets/image2.jpg",
+               image_name="mountain")
+image3 = Image(image_link="/static/assets/github-round.png",
+               image_name="github")
+seed.extend([image1, image2, image3])
+
+curator = Role(role_name="curator", permissions=[
+               "create/read/update/delete image", "read/update gallery", "read content"])
+writer = Role(role_name="writer", permissions=[
+              "create/read/update/delete content", "read gallery", "read image"])
+manager = Role(role_name="manager", permissions=[
+               "create/read/update/delete image", "create/read/update/delete content", "create/read/update/update gallery", "create/read/update/delete blacklist"])
+admin = Role(role_name="admin", permissions=["create/read/update/delete image", "create/read/update/delete content",
+                                             "create/read/update/update gallery", "create/read/update/delete blacklist", "create/read/update/delete admin"])
+seed.extend([curator, writer, manager, admin])
+
 for i in range(10):
     header = Header(header_text=f'This is header #{i}')
     para = Paragraph(paragraph_text=f'A paragraph for header #{i}')
-    header.h_image = image
+    header.h_image = image1
     header.h_paragraph = para
     seed.append(header)
     seed.append(para)
