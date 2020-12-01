b4ced47bbd44cd0038bc92197f320cd216c16f15
Tim <tsclay9@gmail.com>
2020-12-01 17:36:15 -0500

    defined recovery link col for Admin model

--- server/models/Admin.py
+++ server/models/Admin.py
@@ -12,14 +12,16 @@ class Admin(db.Model):
     username = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(128), unique=False, nullable=False)
+    recovery_link = db.Column(db.Text, unique=False, nullable=True)
     active = db.Column(db.Boolean(), default=False, nullable=False)
     last_logged_in = db.Column(db.DateTime(False), nullable=True)
 
-    def __init__(self, first_name, last_name, email, username, password, role, last_logged_in):
+    def __init__(self, first_name, last_name, email, username, password, role, recovery_link, last_logged_in):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.username = username
         self.password = password
+        self.recovery_link = recovery_link
         self.role = role
         self.last_logged_in = last_logged_in
