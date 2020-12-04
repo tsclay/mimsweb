0f8f9cc1c44f4bd0c0254989c46c88a148652a62
Tim <tsclay9@gmail.com>
2020-12-04 11:18:06 -0500

    remove unneeded script

--- scripts/start_dev.sh
+++
@@ -1,5 +0,0 @@
-echo -e "PORT: "
-read PORT
-echo "============== Starting Flask Dev Container =============="
-echo "Access Docker container app @ http://0.0.0.0:$PORT"
-docker run -p $PORT:3000 --rm -v $(PWD):/code flask:dev
