99f932e861366cad3ae9bad0d923502bacab8c25
Tim <tsclay9@gmail.com>
2020-12-04 16:41:24 -0500

    created client_resources db model for assets and content that the client needs

---
+++ server/models/Client_Resources.py
@@ -0,0 +1,20 @@
+from server.db import db
+
+
+class Client_Resources(db.Model):
+    __tablename__ = 'client_resources'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content_id = db.Column(db.Integer, db.ForeignKey(
+        'headers.id'), nullable=True)
+    gallery_id = db.Column(db.Integer, db.ForeignKey(
+        'gallery_info.id'), nullable=True)
+    linked_content = db.relationship(
+        'Header', passive_deletes=True, foreign_keys=[content_id], lazy=True, uselist=False)
+    linked_gallery = db.relationship(
+        'Gallery_Info', passive_deletes=True, foreign_keys=[gallery_id], lazy=True, uselist=False)
+
+    # cascade="all, delete" on gallery_info conflicts with update func
+
+    def __init__(self):
+        pass
