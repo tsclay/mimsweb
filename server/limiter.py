1663bd3b13d0dea3d12d40fc51702e2eec4541ba
Tim <tsclay9@gmail.com>
2021-01-06 18:14:18 -0500

    register redis with limiter

--- server/limiter.py
+++ server/limiter.py
@@ -1,5 +1,9 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import os
 
 limiter = Limiter(key_func=get_remote_address,
-                  default_limits=["200 per day", "50 per hour"])
+                  default_limits=["200 per day", "50 per hour"],
+                  storage_uri=os.environ["REDIS_URI"])
+
+# default_limits=["200 per day", "50 per hour"]
