ad14b7939b22b99d920ec6a421aa2084cbc24eb5
Tim Clay <tsclay9@gmail.com>
2021-03-15 20:23:02 -0400

    fix ip grabbing func

--- server/limiter.py
+++ server/limiter.py
@@ -4,18 +4,18 @@ from flask_limiter.util import get_remote_address
 import os
 
 
-def get_remote_address_fix():
-    # trusted_proxies = {'172.22.0.1'}  # define your own set
-    # route = request.access_route + [request.remote_addr]
-    # print("here is the route", route)
-    # remote_addr = next((addr for addr in reversed(route)
-    #                     if addr not in trusted_proxies), request.remote_addr)
-    remote_addr = request.headers['X-Real-IP']
+# def get_remote_address_fix():
+# trusted_proxies = {'172.22.0.1'}  # define your own set
+# route = request.access_route + [request.remote_addr]
+# print("here is the route", route)
+# remote_addr = next((addr for addr in reversed(route)
+#                     if addr not in trusted_proxies), request.remote_addr)
+# remote_addr = request.headers['X-Real-IP']
 
-    return remote_addr
+# return remote_addr
 
 
-limiter = Limiter(key_func=get_remote_address_fix,
+limiter = Limiter(key_func=get_remote_address,
                   default_limits=["200 per day", "50 per hour"],
                   storage_uri=os.environ["REDIS_URI"])
 
